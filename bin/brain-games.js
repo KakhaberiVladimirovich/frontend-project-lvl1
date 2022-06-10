5305d600f26d9011f162f9f45176bd34beb3c240
Бугианишвили <skulaxo98@mail.ru>
2022-06-10 17:32:27 +0300

    step4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
-console.log("Welcome to the Brain Games!");
+
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+// Wait for user's response.
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello ${userName}!`);
