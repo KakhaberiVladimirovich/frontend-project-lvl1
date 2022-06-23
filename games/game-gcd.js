01ee27b3a8a020452dcb8ff42d75e75002bf14cc
Бугианишвили <skulaxo98@mail.ru>
2022-06-23 22:06:34 +0300

    fixes4

--- games/game-gcd.js
+++ games/game-gcd.js
@@ -1,6 +1,22 @@
 import basisOfGames from '../src/index.js';
 import getRandomIn from '../src/random.js';
 
+const gcdt = (randomNamber1, randomNamber2) => {
+  let result = '';
+  let rand1 = randomNamber1;
+  let rand2 = randomNamber2;
+
+  while (rand1 !== rand2) {
+    if (rand1 > rand2) {
+      rand1 -= rand2;
+    } else {
+      rand2 -= rand1;
+    }
+  }
+  result = rand1;
+  return result;
+};
+
 const gcd = () => {
   const noteToGame = 'Find the greatest common divisor of given numbers.';
 
@@ -9,18 +25,7 @@ const gcd = () => {
     const random2 = getRandomIn(100);
     const expression = `${random1} ${random2}`;
 
-    let result = '';
-    let Number1 = random1;
-    let Number2 = random2;
-    while (Number1 !== Number2) {
-      if (Number1 > Number2) {
-        Number1 -= Number2;
-      } else {
-        Number2 -= Number1;
-      }
-    }
-    result = Number1;
-    return [expression, result];
+    return [expression, gcdt(random1, random2)];
   };
   basisOfGames(noteToGame, gcd1);
 };
