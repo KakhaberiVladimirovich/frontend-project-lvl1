d6612391bfbe521143119530b79a1f3f850cee65
Бугианишвили <skulaxo98@mail.ru>
2022-06-24 10:02:39 +0300

    fixec5

--- games/game-prime.js
+++ games/game-prime.js
@@ -3,9 +3,7 @@ import getRandomIn from '../src/random.js';
 
 const primeNamb = (random) => {
   let rool = true;
-  if (random === 1) {
-    rool = true;
-  } else if (random === 0 || random === 2 || random === 1) {
+  if (random === 0 || random === 2 || random === 1) {
     rool = false;
   } else {
     for (let i = 2; i < random; i += 1) {
