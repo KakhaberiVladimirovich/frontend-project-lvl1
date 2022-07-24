967380e606fb63108fa2cd866d1eac1b4e4fc9f9
Бугианишвили <skulaxo98@mail.ru>
2022-07-24 21:23:46 +0300

    fixec

--- src/games/gameProgression.js
+++ src/games/gameProgression.js
@@ -3,28 +3,29 @@ import getRandomNumber from '../helpers.js';
 
 const description = 'What number is missing in the progression?';
 
-const getGenerateRound = (number1, number2, long) => {
+const getProgression = (number1, number2, long) => {
   const consistency = [number1];
   let nextNumber = number1;
   for (let i = 0; i <= long; i += 1) {
     nextNumber += number2;
     consistency.push(nextNumber);
   }
-  const result = consistency.splice(number1, 1, '..');
-  const resultString = result.toString();
-  const joinString = consistency.join(' ');
+  return consistency;
+};
+
+const generateRound = () => {
+  const number1 = getRandomNumber(0, 10);
+  const number2 = getRandomNumber(0, 10);
+  const long = 10;
+
+  const result = getProgression(number1, number2, long);
+  const resultString = result.splice(number1, 1, '..').toString();
+  const joinString = result.join(' ');
   return [joinString, resultString];
 };
 
 const startGame = () => {
-  const substituteARandomNumber = () => {
-    const number1 = getRandomNumber(0, 10);
-    const number2 = getRandomNumber(0, 10);
-
-    return getGenerateRound(number1, number2, 10);
-  };
-
-  start(description, substituteARandomNumber);
+  start(description, generateRound);
 };
 
 export default startGame;
