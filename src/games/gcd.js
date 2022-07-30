470e83a3c5d19fa6c00770b2523d879466f71bff
Бугианишвили <skulaxo98@mail.ru>
2022-07-30 21:41:46 +0300

    fixec 6

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,18 +3,13 @@ import getRandomNumber from '../helpers.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-const getGCD = (randomNumber1, randomNumber2) => {
-  if (randomNumber1 !== 0) {
-    getGCD(randomNumber2 % randomNumber1);
-  }
-  return randomNumber2.toString();
-};
+const nod = (number1, number2) => (number1 !== 0 ? nod(number2 % number1, number1) : number2);
 
 const generateRound = () => {
   const number1 = getRandomNumber(0, 100);
   const number2 = getRandomNumber(0, 100);
   const question = `${number1} ${number2}`;
-  const result = getGCD(number1, number2);
+  const result = nod(number1, number2);
   const answer = result.toString();
   return [question, answer];
 };
